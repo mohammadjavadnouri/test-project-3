910b3d81d9f24376941903e140dbc6433342eaab
Your Name <you@example.com>
2020-12-06 17:39:24 +0330

    add webpack (spent 8 fucking hours due to errors)

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+console.log("index.jssss is runww:0");
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="naaav">
+            <nav>
+                <a href="#">صفحه اصلی</a>
+                <a href="#">درباره ما</a>
+                <a href="#">تماس باما</a>
+                <a href="#">عضویت داروخانه ها</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Home;
