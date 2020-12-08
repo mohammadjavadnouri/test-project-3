b6051f65df40739093a4347d0b2307555651e7db
Your Name <you@example.com>
2020-12-08 22:08:49 +0330

    home page V2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row, Button, Nav, Image, Form } from 'react-bootstrap';
+import { Container, Col, Row, Button, Nav, Image, Form, Figure } from 'react-bootstrap';
 import logo from "../../assets/Group152.svg";
 import doctor from "../../assets/doctor_two_color.svg";
 import playMobile from "../../assets/playMobile.png";
@@ -20,15 +20,13 @@ import callCenter from "../../assets/callCenter.png";
 import bazzarBlackButton from "../../assets/bazzarBlackButton.png";
 import sibAppBlackButton from "../../assets/sibAppBlackButton.png";
 
-const Home = () => {
-    return(
-        <Container  style={{maxWidth: "100%"}}>
-
-            <Row>
-                <Col md={2} style={{ textAlign:"right"}}>
+const Home = () => (
+        <Container className="home">
+            <Row className="nav">
+                <Col md={2} className="logo">
                     <Image src={logo} alt="Logo" />
                 </Col>
-                <Col md={8} style={{ textAlign:"center" }}>
+                <Col md={8} className="links">
                     <Nav className="justify-content-center" activeKey="/home" >
                         <Nav.Item>
                             <Nav.Link href="/home">صفحه اصلی</Nav.Link>
@@ -44,128 +42,154 @@ const Home = () => {
                         </Nav.Item>
                     </Nav>
                 </Col>
-                <Col md={2} style={{ alignContent:"center", alignItems:"center", display:"flex"}}>
+                <Col md={2} className="button">
                     <Button variant="primary">دانلود درمانیتو</Button>
                 </Col>
             </Row>
 
-            <Row style={{ marginTop:"10px"}}>
-                <Col sm={6} style={{backgroundColor: "yellow"}}>
-                    <Row>
+            <Row className="second" >
+                <Col sm={6} className="right">
+                    <Row className="title">
                         <h4>اپلیکیشن درمانیتو</h4>
                     </Row>
                     <Row>
                         <p>درمانیتو سامانه هوشمند درمان و سلامت</p>
                     </Row>
                     <Row>
-                        <Button>دانلود درمانیتو</Button>
-                        <Button>عضویت داروخانه ها</Button>
+                        <Button className="button">دانلود درمانیتو</Button>
+                        <Button className="button">عضویت داروخانه ها</Button>
                     </Row>
                 </Col>
-                <Col sm={6} style={{backgroundColor: "aqua"}}>
+                <Col sm={6} className="left">
                     <Image src={doctor}/>
+                    <svg xmlns= "http://www.w3.org/2000/svg" width= "700" className="bg"
+                    height= "618.916" viewBox= "0 0 943.749 618.916" > 
+                        <path id= "Path_15701"
+                        data-name= "Path 15701" d=
+                        "M-154.953,356.268c61.376-70.487,182.582,20.7,358.2-19.207,152.109-34.582,160.86-126.439,282.392-121.884,88.789,3.31,185.541,54.578,238.714,143.895C783,457.551,761.68,557.81,755.7,582.139c-26.713,109.124-106,166.687-133.968,186.348-115.24,80.953-244.555,66.767-306.592,59.992C82.1,802.957-167.386,636.583-178.495,455.7-179.971,432.021-182.658,388.1-154.953,356.268Z"
+                        transform= "translate(179.337 -215.014)" fill= "#e6f3ff" opacity= "0.41" /> 
+                    </svg>
                 </Col>
             </Row>
 
-            <Row style={{ marginTop:"10px"}}>
-                <Col sm={6} style={{backgroundColor: "aqua"}}>
-                    <Image src={playMobile}/>
+            <Row className="third" >
+                <Col sm={6} >
+                    <Image className="right" src={playMobile}/>
                 </Col>
-                <Col sm={6} style={{backgroundColor: "yellow", textAlign:"right"}}>
-                    <Row>
+                <Col sm={6} className="left">
+                    <Row className="title">
                         <h4>درباره درمانیتو</h4>
                     </Row>
                     <Row>
-                        <p>.درمانیتو یک سامانه است که امکان سفارش دارو، محصولات آرایشی بهداشتی نوبت گیری و مشاوره پزشکان را به راحتی و بصورت آنلاین فراهم کرده است</p>
+                        <p>درمانیتو یک سامانه است که امکان سفارش دارو، محصولات آرایشی بهداشتی نوبت گیری و مشاوره پزشکان را به راحتی و بصورت آنلاین فراهم کرده است.</p>
                     </Row>
-                    <Row>
-                        <Col><Image src={fastSearch}/></Col>
-                        <Col><Image src={efficient}/></Col>
-                        <Col><Image src={easy}/></Col>
-                        <Col><Image src={search}/></Col>
+                    <Row className="footer">
+                        <Figure>
+                            <Figure.Image src={fastSearch} />
+                            <Figure.Caption>جستجو سریع </Figure.Caption>
+                        </Figure>
+                        <Figure>
+                            <Figure.Image src={efficient} />
+                            <Figure.Caption>به صرفه</Figure.Caption>
+                        </Figure>
+                        <Figure>
+                            <Figure.Image src={easy} />
+                            <Figure.Caption>راحت</Figure.Caption>
+                        </Figure>
+                        <Figure>
+                            <Figure.Image src={search} />
+                            <Figure.Caption>جستجو سریع</Figure.Caption>
+                        </Figure>
                     </Row>
                 </Col>
             </Row>
 
-            <Row style={{ marginTop:"10px"}}>
-                <Container style={{maxWidth: "100%"}}>
+            <Row className="fourth">
+                <Container >
                     <Row>
-                        <Col sm={12} style={{backgroundColor: "lightGreen", display:"block", textAlign:"right"}}>
-                        <h4>امکانات درمانیتو</h4>
+                        <Col sm={12}  >
+                            <h4 className="up">امکانات درمانیتو</h4>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col sm={6} style={{backgroundColor: "yellow"}}>
-                            <Image src={drugStore} />
-                            <Image src={store} />
-                            <Image src={visitOnline} />
-                            <Image src={nobatDehi} />
+                    <Row >
+                        <Col sm={6} >
+                            <Image className="image" src={drugStore} />
+                            <Image className="image" src={store} />
+                            <Image className="image" src={visitOnline} />
+                            <Image className="image" src={nobatDehi} />
                         </Col>
-                        <Col sm={6} style={{backgroundColor: "aqua", textAlign:"right"}}>
+                        <Col sm={6} className="down__left" >
                             <h5>داروخانه آنلاین</h5>
-                            <p>در سرویس داروخانه شما میتوانید نسخه دارویی یا محصول بهداشتی خودتون رو برای پیدا شدن به داروخانه و فروشگاه ها سفارش بدید </p>
-                            <div>
-                                <p>قدم اول</p>
-                                <p>وارد بخش سفارش با عکس نسخه شوید و عکس و توضیحات را وارد کنید</p>
-                                <p>قدم دوم</p>
-                                <p>بعد از ارسال عکس نسخه، باید آدرس محل خودتون رو وارد کنید</p>
-                                <p>قدم سوم</p>
-                                <p>اگر نتیجه جستجوی نسخه موفقیت آمیز باشه باید منتظر تحویل سفارش باشید</p>
+                            <p>در سرویس داروخانه شما میتوانید نسخه دارویی ی ا محصول بهداشتی خودتون رو برای پیدا شدن به داروخانه و فروشگاه ها سفارش بدید.</p>
+                            <div className="steps">
+                                <i className="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                                <p className="step">قدم اول</p>
+                                <p className="step__description">وارد بخش سفارش با عکس نسخه شوید و عکس و توضیحات را وارد کنید.</p>
+                                <i className="fa fa-arrow-circle-down" aria-hidden="true"></i>
+                                <p className="step">قدم دوم</p>
+                                <p className="step__description">بعد از ارسال عکس نسخه، باید آدرس محل خودتون رو وارد کنید.</p>
+                                <i className="fa fa-check-circle" aria-hidden="true"></i>
+                                <p className="step">قدم سوم</p>
+                                <p className="step__description">اگر نتیجه جستجوی نسخه موفقیت آمیز باشه باید منتظر تحویل سفارش باشید.</p>
                             </div>
                         </Col>
                     </Row>
                 </Container>
             </Row>
 
-            <Row style={{ marginTop:"10px", backgroundColor: "lightBlue"}}>
-                <Col sm={6} style={{backgroundColor: "aqua", textAlign:"right"}}>
+            <Row className="fifth" >
+                <Col sm={6} className="right" >
                     <h4>دانلود اپلیکیشن درمانیتو</h4>
                     <p>برای دریافت لینک دانلود شماره موبایل خود را وارد کنید</p>
                     <Form>
                         <Form.Group controlId="number">
-                            <Form.Control type="number" placeholder="شماره همراه را وارد کنید 09000000000" />
+                            <Form.Control className="input" type="number" placeholder="شماره همراه را وارد کنید 09000000000" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button className="submit" variant="primary" type="submit">
                             بفرست
                         </Button>
                     </Form>
-                    <Image src={downloadBlueButton}/>
-                    <Image src={bazzarBlueButton}/>
-                    <Image src={sibAppBlueButton}/>
+                    <div className="store">
+                        <Image className="store__icon" src={downloadBlueButton}/>
+                        <Image className="store__icon" src={bazzarBlueButton}/>
+                        <Image className="store__icon" src={sibAppBlueButton}/>
+                    </div>
                 </Col>
-                <Col sm={6} style={{backgroundColor: "yellow", textAlign:"center"}}>
-                    <Image src={damanitoApp}/>
+                <Col sm={6} className="left" >
+                    <Image className="left__image"  src={damanitoApp}/>
                 </Col>
             </Row>
 
-            <Row style={{ marginTop:"10px", textAlign:"right"}}>
-                <Col sm={6} style={{backgroundColor: "yellow"}}>
-                    <h4>عضویت داروخانه یا مراکز درمانی</h4>
+            <Row className="sixth" >
+                <Col sm={6} className="right">
+                    <h4 className="title">عضویت داروخانه یا مراکز درمانی</h4>
                     <p>اگر به عنوان داروخانه یا مرکز درمانی قصد دارید در روند درمان و کسب کار خودتان تغییری ایجاد کنید درمانیتو منتظر شماست</p>
                     <Form>
                         <Form.Group controlId="number">
-                            <Form.Control type="number" placeholder="شماره همراه را وارد کنید 09000000000" />
+                            <Form.Control className="input" type="number" placeholder="شماره همراه را وارد کنید 09000000000" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button className="button" variant="primary" type="submit">
                             ثبت نام سریع
                         </Button>
                     </Form>
                 </Col>
-                <Col sm={6} style={{backgroundColor: "aqua"}}>
+                <Col sm={6} className="left">
                     <Image src={healthDoctor} />
                 </Col>
             </Row>
-            <Row style={{ marginTop:"10px"}}>
-                <Col sm={6} style={{backgroundColor: "aqua", textAlign:"right"}}>
+
+            <Row className="seventh" >
+                <Col sm={6} className="right" >
                     <h5>پشتبانی درمانیتو</h5>
                     <p>پاسخگویی روزهای کاری از 9 صبح تا 9 شب</p>
                 </Col>
-                <Col sm={6} style={{backgroundColor: "yellow"}}>
+                <Col sm={6} className="left" >
                     <Image src={callCenter} />
                 </Col>
             </Row>
-            <Row style={{ marginTop:"10px"}}>
-                <Col sm={4} style={{backgroundColor: "salmon", textAlign:"right"}}>
+
+            <Row className="footer" >
+                <Col sm={4} >
                     <Image src={logo}/>
                     <h5>سوپر اپلیکیشن حوزه درمان و سلامت</h5>
                     <p>تمام حقوق مادی و معنوی این وبسایت متعلق به درمانیتو است</p>
@@ -173,28 +197,27 @@ const Home = () => {
                     <i href="#" className="fa fa-twitter"></i>
                     <i href="#" className="fa fa-instagram"></i>
                 </Col>
-                <Col sm={2} style={{backgroundColor: "lightGreen", textAlign:"right"}}>
+                <Col sm={2} >
                     <h5>خدمات درمانیتو</h5>
                     <p>داروخانه آنلاین</p>
                     <p>فروشگاه آنلاین</p>
                     <p>ویزیت آنلاین</p>
                     <p>پرونده پزشکی</p>
                 </Col>
-                <Col sm={2} style={{backgroundColor: "lightBlue", textAlign:"right"}}>
+                <Col sm={2}>
                     <h5>صفحات درمانیتو</h5>
                     <p>تماس باما</p>
                     <p>درباره ما</p>
                     <p>سوالات متداول</p>
                     <p>قوانین و شرایط</p>
                 </Col>
-                <Col sm={4} style={{backgroundColor: "wheat", textAlign:"right"}}>
+                <Col sm={4} >
                     <h5>دانلود درمانیتو</h5>
-                    <Image src={bazzarBlackButton}/>
-                    <Image src={sibAppBlackButton}/>
+                    <Image className="store" src={bazzarBlackButton}/>
+                    <Image className="store" src={sibAppBlackButton}/>
                 </Col>
             </Row>
-        </Container>
-    )
-}
+    </Container>
+    );
 
 export default Home;
